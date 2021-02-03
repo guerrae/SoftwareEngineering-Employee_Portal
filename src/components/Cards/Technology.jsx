221d289fb89acab50db8115f8682e420384e832a
guerrae <warx@ymail.com>
2021-02-03 12:37:06 -0600

    Route Syntax Fix

--- src/components/Cards/Technology.jsx
+++ src/components/Cards/Technology.jsx
@@ -9,7 +9,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import techImg from "./technologyLogo.png"//w512 h422
-import {Link} from "react-router-dom;"
+import {Link} from "react-router-dom";
 
 const theme = createMuiTheme({
   palette: {
